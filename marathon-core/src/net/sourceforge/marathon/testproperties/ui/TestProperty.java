6032b608779b349473906a1aea52911889524081
Sampathkumar R <sampathkumar.r@jaliansystems.com>
2011-03-11 11:31:36 +0530

    Added: hashcode and equals methods to TestProperty

--- marathon-core/src/net/sourceforge/marathon/testproperties/ui/TestProperty.java
+++ marathon-core/src/net/sourceforge/marathon/testproperties/ui/TestProperty.java
@@ -2,11 +2,11 @@ package net.sourceforge.marathon.testproperties.ui;
 
 public class TestProperty {
 
-    static enum PropertyType {
+    public static enum PropertyType {
         INTEGER, STRING
     }
 
-    static enum DisplayType {
+    public static enum DisplayType {
         TEXTBOX, TEXTFIELD
     }
 
@@ -55,6 +55,41 @@ public class TestProperty {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    @Override public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((display == null) ? 0 : display.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TestProperty other = (TestProperty) obj;
+        if (display != other.display)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (type != other.type)
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
     }
 
 }
