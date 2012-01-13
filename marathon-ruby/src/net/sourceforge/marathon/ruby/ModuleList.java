776a562a08785f07f9d72de37d26959e94a558b1
Dakshinamurthy Karra <dakshinamurthy.karra@jaliansystems.com>
2012-01-13 14:34:42 +0530

    CLOSED - # 20: Syntax errors in module files causes grief http://svn/mantis/view.php?id=20Signed-off-by: Dakshinamurthy Karra <dakshinamurthy.karra@jaliansystems.com>

--- marathon-ruby/src/net/sourceforge/marathon/ruby/ModuleList.java
+++ marathon-ruby/src/net/sourceforge/marathon/ruby/ModuleList.java
@@ -153,6 +153,7 @@ public class ModuleList {
         Lexer lexer = new Lexer();
         lexer.setSource(LexerSource.getSource(file.getName(), new FileReader(file), new ParserConfiguration()));
         lexer.setPreserveSpaces(true);
+        lexer.setWarnings(new Parser.NullWarnings());
         String doc;
         int token = -1;
         Properties props = new Properties();
