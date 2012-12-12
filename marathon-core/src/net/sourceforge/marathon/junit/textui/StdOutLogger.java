d6e7e8b000c3827bf676fdf3a3e04067e88b2f8d
Dakshinamurthy Karra <dakshinamurthy.karra@jaliansystems.com>
2012-12-12 18:43:29 +0530

    Fixes: 1. Implemented StdOutLogger 2. Removed printStackTrace when a Context menu can't be loaded.

--- marathon-core/src/net/sourceforge/marathon/junit/textui/StdOutLogger.java
+++ marathon-core/src/net/sourceforge/marathon/junit/textui/StdOutLogger.java
@@ -4,21 +4,33 @@ import net.sourceforge.marathon.api.ILogger;
 
 public class StdOutLogger implements ILogger {
     public void info(String module, String message) {
+        info(module, message, null);
     }
 
     public void info(String module, String message, String description) {
+        p("INFO", module, message, description);
     }
 
     public void warning(String module, String message) {
+        warning(module, message, null);
     }
 
     public void warning(String module, String message, String description) {
+        p("WARNING", module, message, description);
     }
 
     public void error(String module, String message) {
+        error(module, message, null);
     }
 
     public void error(String module, String message, String description) {
+        p("ERROR", module, message, description);
+    }
+
+    private void p(String type, String module, String message, String description) {
+        System.err.println(type + "<" + module + ">: " + message);
+        if(description != null)
+            System.err.println(description);
     }
 
 }
